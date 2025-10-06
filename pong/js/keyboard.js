e7b82d1ad5b15861da5b3c95b9a979f127017f9a
Rblye22 <rpblye@email.neit.edu>
2025-10-05 20:01:11 -0400

    push assignment

---
+++ pong/js/keyboard.js
@@ -0,0 +1,13 @@
+//array of keys
+var keys = [];
+
+//keydown code
+document.addEventListener(`keydown`, (e)=>{
+    keys[e.key]=true;
+    //console.log(e.key)
+})
+
+//keyup code
+document.addEventListener(`keyup`, (e)=>{
+    keys[e.key]=false;
+})
